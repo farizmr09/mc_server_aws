f6c7e7cf5f1118f043d7fd042085ad44ee251903
farizmr09 <farizmuhridwan@gmail.com>
2024-09-05 10:43:40 +0700

    Initial commit

---
+++ test/mc_server.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const McServer = require('../lib/mc_server-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mc_server-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new McServer.McServerStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
